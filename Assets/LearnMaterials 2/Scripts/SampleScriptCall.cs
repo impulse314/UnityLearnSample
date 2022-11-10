604f80c0f3bab032e7a56f30b2de2940276f98fa
Zahar Tsytsura <mcrest88@gmail.com>
2022-11-10 21:46:32 +0700

    Материалы для Скриптинга
    Сцена для демонстрации скриптов, скрипт SampleScriptCall для запуска всех кастомных скриптов

---
+++ Assets/LearnMaterials 2/Scripts/SampleScriptCall.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace LearnMaterials_2.Scripts
+{
+    public class SampleScriptCall : MonoBehaviour
+    {
+        [ContextMenu("Активировать модули")]
+        public void Use()
+        {
+            var objects = FindObjectsOfType<SampleScript>();
+            foreach(var script in objects)
+            {
+                script.Use();
+            }
+        }
+    }
+}
