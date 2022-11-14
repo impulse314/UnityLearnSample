eeb65541a52a25ca81e003a43a09de2bfdfc15bb
Zahar Tsytsura <mcrest88@gmail.com>
2022-11-14 15:41:19 +0700

    Revert "Merge branch 'impulse314_branch'"
    This reverts commit 8b5171e2e202e0fb08998fcec31f3fdf8ee8c524, reversing
    changes made to 23b33ad14ac34eff29047df8a80b05c0a6cbc478.

--- Assets/LearnMaterials 2/Scripts/TransparentModule.cs
+++ Assets/LearnMaterials 2/Scripts/TransparentModule.cs
@@ -6,31 +6,24 @@ using UnityEngine;
 [HelpURL("https://docs.google.com/document/d/1Cmm__cbik5J8aHAI6PPaAUmEMF3wAcNo3rpgzsYPzDM/edit?usp=sharing")]
 public class TransparentModule : MonoBehaviour
 {
-    [Header("TransparentМодуль")]
-
-
-    [SerializeField]
     private float changeSpeed;
-    [SerializeField]
+
     private float defaultAlpha;
-    //[SerializeField]
-    //private Material mat;
-    [SerializeField]
+    private Material mat;
     private bool toDefault;
-    Renderer renderer;
 
     private void Start()
     {
-       renderer= GetComponent<Renderer>();
-        defaultAlpha = renderer.material.color.a;
+        mat = GetComponent<Renderer>().material;
+        defaultAlpha = mat.color.a;
         toDefault = false;
     }
-    [ContextMenu("Начать настройку прозрачности объектов")]
+
     public void ActivateModule()
     {
         float target = toDefault ? defaultAlpha : 0;
         StopAllCoroutines();
-        StartCoroutine(ChangeTransparencyCoroutine(new Color(renderer.material.color.r, renderer.material.color.g, renderer.material.color.b, target)));
+        StartCoroutine(ChangeTransparencyCoroutine(new Color(mat.color.r, mat.color.g, mat.color.b, target)));
         toDefault = !toDefault;
     }
 
@@ -39,19 +32,17 @@ public class TransparentModule : MonoBehaviour
         toDefault = true;
         ActivateModule();
     }
-    [SerializeField]
-    float t = 0;
+
     private IEnumerator ChangeTransparencyCoroutine(Color target)
     {
-        Color start = renderer.material.color;
-        t = 0;
+        Color start = mat.color;
+        float t = 0;
         while (t < 1)
         {
             t += Time.deltaTime * changeSpeed;
-            renderer.material.color = Color.Lerp(start, target, t);
-            Debug.Log(renderer.material.color);
+            mat.color = Color.Lerp(start, target, t);
             yield return null;
         }
-        renderer.material.color = target;
+        mat.color = target;
     }
 }
